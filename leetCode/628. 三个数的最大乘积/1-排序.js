72453a72504e71d5040b640c2e0e6ff6359a9adc
xie926 <1187904163@qq.com>
2020-07-07 19:38:36 +0800

    20200707

--- leetCode/628. 三个数的最大乘积/1-排序.js
+++ leetCode/628. 三个数的最大乘积/1-排序.js
@@ -1,7 +1,30 @@
-var maximumProduct = function(nums) {
-  nums.sort((a,b) => a-b)
-  let index =nums.length - 1;
-  let result1 = nums[0] * nums[1] * nums[index]
-  let result2 = nums[index] * nums[index-1] * nums[index-2]
-  return Math.max(result2, result1)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findShortestSubArray = function(nums) {
+  let map = new Map()
+  for(let i = 0; i < nums.length; i++){
+      if(!map.has(nums[i])){
+          map.set(nums[i], 1)
+      }else(
+          map.set(nums[i], map.get(nums[i]) + 1)
+      )
+  }
+  let countMax = Math.max.apply(null, [...map.values()]), arr=[]
+  // console.log(countMax)
+  for(let n of map.keys()){
+      if(map.get(n) === countMax){
+          arr.push(n)
+      }
+  }
+  // console.log(arr)
+  let result = nums.length;
+  for(let i = 0; i < arr.length; i++){
+    let flag = nums.lastIndexOf(arr[i]) - nums.indexOf(arr[i]) + 1
+    result = Math.min(result, flag)
+  }
+  return result
 };
+
+console.log(findShortestSubArray([1,2,2,3,1]))
