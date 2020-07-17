ec842481f0d23b744df20173daec5a681357853c
xie926 <1187904163@qq.com>
2020-07-17 10:09:03 +0800

    20200717

---
+++ leetCode/819. 最常见的单词/index.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+var mostCommonWord = function (paragraph, banned) {
+  paragraph = paragraph.toLowerCase().replace(/[\!|\?|\'|\,|\;|\.]/g, ' ');
+  let arr = paragraph.split(/\ +/g), map = new Map()
+  arr.forEach((item) => {
+      if (!map.has(item)) {
+          map.set(item, 1)
+      } else {
+          map.set(item, map.get(item) + 1)
+      }
+  })
+  banned.forEach((item) => {
+      if (map.has(item)) {
+          map.delete(item)
+      }
+  })
+  let max = Math.max.apply(null, [...map.values()]), result = [];
+  console.log(max)
+  for (let [key, value] of map) {
+      if (value == max) {
+          return key
+      }
+  }
+};
